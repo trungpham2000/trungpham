1fa1f05e1d290e0a58c6501a2a66e9442a625662
github_username <you@example.com>
2023-05-03 02:03:16 +0700

    migrate

---
+++ database/migrations/2023_05_02_165044_create_films_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 256);
+            $table->integer('categorys_id');
+            $table->string('image', 256);
+            $table->integer('actors_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
---
+++ database/migrations/2023_05_02_171618_create_actors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 256);
+            $table->string('image', 256);
+            $table->date('birthday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actors');
+    }
+};
